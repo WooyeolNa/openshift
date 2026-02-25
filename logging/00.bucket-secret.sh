32da9a22988f7940b4679ba5c0414f40f29b1d25
WooyeolNa <skdnduf909@gmail.com>
2026-02-25 11:07:52 +0900

    Upload all YAML configuration files from subdirectories

--- logging/00.bucket-secret.sh
+++
@@ -1,7 +0,0 @@
-oc create secret generic logging-loki-s3 \
-  --from-literal=bucketnames="minio-storage" \
-  --from-literal=endpoint="http://172.1.157.150:9000" \
-  --from-literal=access_key_id="eDQ6BRKwLb7VS7lOPYsI" \
-  --from-literal=access_key_secret="h8vU2Whg6XaShU8KT99Sh7G5Eryp2INxlTUobKmL" \
-  --from-literal=region="kor-1" \
-  -n openshift-logging
